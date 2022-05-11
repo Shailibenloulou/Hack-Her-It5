8e1a2602db5846a70d04f4e8225b3689554420a9
חגית אורנשטיין <chagito3440@gmail.com>
2022-05-11 18:46:24 +0300

    first

---
+++ Clean_App/Models/userControllerClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clean_App.Models
+{
+    public class userControllerClass
+    {
+     public int Number { get; set; }
+     public string UserName { get; set; }
+        public string Password { get; set}
+    }
+}
