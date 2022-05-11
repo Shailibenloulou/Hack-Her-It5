20a7c812762586318fb1e8bda37600c89c8be819
שילי בן לולו <shaili@g.jct.ac.il>
2022-05-11 23:00:53 +0300

    hvfeg

---
+++ Clean_App/Models/Group.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clean_App.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string> Participants { get; set; }
+        public Location area { get; set; }
+        public Boolean IsFull { get; set; }
+        public Boolean IsDone { get; set; }
+
+        public Group()
+        {
+            
+        }
+    }
+}
